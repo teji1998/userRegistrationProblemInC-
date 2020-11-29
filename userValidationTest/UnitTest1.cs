a53b44f7d9bd4ce19a5cd5beac7e3da9382c7be6
“teji1998” <tejasvinirpk@gmail.com>
2020-11-29 07:01:10 +0530

    Added:Test cases for validation of email

--- userValidationTest/UnitTest1.cs
+++ userValidationTest/UnitTest1.cs
@@ -234,7 +234,41 @@ namespace userValidationTest
             Assert.IsFalse(result);
         }
 
-        
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void givenEmails_WhenAreValid_ShouldReturnTrue(string email)
+        {
+            bool result = userValidation.emailIdValidation(email);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow("abc.com")]
+        [DataRow("abc@.com.my")]
+        [DataRow("abc123@gmail.a")]
+        [DataRow("abc123@.com")]
+        [DataRow("abc123@.com.com")]
+        [DataRow(".abc@abc.com")]
+        [DataRow("abc@%*.com")]
+        [DataRow("abc()*@gmail.com")]
+        [DataRow("abc..2002@gmail.com")]
+        [DataRow("abc.@gmail.com")]
+        [DataRow("abc@gmail.com.1a")]
+        [DataRow("abc@gmail.com.aa.au")]
+        [DataRow(" ")]
+        public void givenEmail_WhenAreNotValid_ShouldReturnFalse(string email)
+        {
+            bool result = userValidation.emailIdValidation(email);
+            Assert.IsFalse(result);
+        }
 
     }
 }
