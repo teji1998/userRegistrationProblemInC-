75133e623026e174681ecfbc6f662ba2700bdd45
“teji1998” <tejasvinirpk@gmail.com>
2020-12-02 05:10:20 +0530

    Added:Code to validate user details using annotations

--- userRegistrationProblem/Program.cs
+++ userRegistrationProblem/Program.cs
@@ -7,67 +7,10 @@ namespace userRegistrationProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the user registration problem ! ");
-
-            UserValidation userValidation = new UserValidation();
-
-            //First name validation
-            Console.WriteLine("Enter your first name.");
-            string firstName = Console.ReadLine();
-            if (!userValidation.nameValidation(firstName))
-            {
-                Console.WriteLine("First name entered is invalid. \nPlease enter a valid first name : ");
-                firstName = Console.ReadLine();
-            }
-            else
-                Console.WriteLine("First name entered is valid");
-
-            //Last name validation
-            Console.WriteLine("Enter your last name.");
-            string lastName = Console.ReadLine();
-            if (!userValidation.nameValidation(lastName))
-            {
-                Console.WriteLine("Last name entered is invalid. \nPlease enter a valid last name : ");
-                lastName = Console.ReadLine();
-            }
-            else
-                Console.WriteLine("Last name entered is valid");
-
-            //Email id validation
-            Console.WriteLine("Enter your email");
-            string email = Console.ReadLine();
-
-           if (!userValidation.emailIdValidation(email))
-            {
-                Console.WriteLine("Email entered is invalid. \nPlease enter a valid email : ");
-                email = Console.ReadLine();
-            }
-            else
-                Console.WriteLine("Email entered is valid"); 
-
-            //Mobile number validation
-            Console.WriteLine("Enter your mobile number");
-            string number = Console.ReadLine();
-            if (!userValidation.mobileNumberValidation(number))
-            {
-                Console.WriteLine("Mobile number entered is invalid. \nPlease enter a valid mobile number : ");
-                number = Console.ReadLine();
-            }
-            else
-                Console.WriteLine("Mobile number entered is valid");
-
-            //Password Validation
-            Console.WriteLine("Enter your password");
-            string password = Console.ReadLine();
-            if (!userValidation.passwordValidation(password))
-            {
-                Console.WriteLine("Password entered is invalid. \nPlease enter a valid password : ");
-                password = Console.ReadLine();
-
-            }
-            else
-                Console.WriteLine("Password entered is valid");
-            
-        }
+            UserValidationTest user = new UserValidationTest();
+            user.Display("Tejaswini", "Kulkarni",  "tejasvinirpk@gmail.com", "91 9920275347", "Teji@1998");
+            user.Display("Tejaswini", "Ku", "teji@% gmail.com", "9920275347", "Teji@");
+        }   
     
     }
 }
---
+++ userRegistrationProblem/UserValidationTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace userRegistrationProblem
+{
+    public class UserValidationTest
+    {
+        public void Display (string firstName, string lastName, string email, string number, string password)
+        {
+            Console.WriteLine("User class validation");
+            Console.WriteLine("---------------------------------");
+            UserDetails userDetails = new UserDetails();
+
+            userDetails.Firstame = firstName;
+            userDetails.LastName = lastName;
+            userDetails.Number = number;
+            userDetails.Email = email;
+            userDetails.Password = password;
+
+            ValidationContext context = new ValidationContext(userDetails, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(userDetails, context, results, true);
+
+            if (!valid)
+            {
+                foreach (ValidationResult TotalResult in results)
+                {
+                    Console.WriteLine("MemberName:{0}", TotalResult.MemberNames.First(), Environment.NewLine);
+                    Console.WriteLine("ErrorMsg::{0}", TotalResult.ErrorMessage, Environment.NewLine);
+                }
+            }
+            else
+            {
+                Console.WriteLine("First Name : " + userDetails.Firstame + "\n" + "Last Name : "  + userDetails.LastName + "\n" + "Mobile Number : " + userDetails.Number + "\n" + "Email : " + userDetails.Email + "\n" + " Password : " + userDetails.Password);
+            }
+
+
+
+        }
+    }
+}
